abc2d4f8abf5b668fec7aa4c1326c6d6f8b444d8
Igor Couto <igor.fcouto@gmail.com>
2021-09-13 01:47:55 -0300

    Fix search algorithm

--- src/search.rs
+++ src/search.rs
@@ -2,17 +2,15 @@ pub fn search(number_to_search: &String, pi: &String) -> Result<u32, String> {
     let number_to_search_bytes = number_to_search.as_bytes();
     let pi_bytes = pi.as_bytes();
 
-    for pi_index in 0..pi_bytes.len() {
-        println!("pi_index: {}", pi_index);
+    'search_inside_pi: for pi_index in 0..pi_bytes.len() {
         if pi_bytes[pi_index] == number_to_search_bytes[0] {
-            for i in 1..number_to_search_bytes.len() {
+            for i in 0..number_to_search_bytes.len() {
                 if number_to_search_bytes[i] != pi_bytes[pi_index + i] {
-                    break;
+                    continue 'search_inside_pi;
                 }
-                return Ok(pi_index as u32);
             }
+            return Ok(pi_index as u32);
         }
     }
-
     Err(String::from("Not found."))
 }
